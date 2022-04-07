521e4c4a618517c86641f99e4bb59d8132436089
TripToCodes <iam.somj@gmail.com>
2022-04-07 09:16:01 -0700

    server setting

---
+++ server/migrations/20220407062348-create-group.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      group_name: {
+        type: Sequelize.STRING
+      },
+      group_description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('groups');
+  }
+};
