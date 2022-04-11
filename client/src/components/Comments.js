6abd1aadbbe1a22570ead76214a4a0a4ab739ae8
TripToCodes <iam.somj@gmail.com>
2022-04-11 05:32:17 -0700

    add article with comments

---
+++ client/src/components/Comments.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import Comment from "./Comment";
+import shortid from "shortid";
+
+// comments axios.get으로 불러오기
+const dummyComments = [
+  {
+    id: 1,
+    nickname: "유어턴",
+    content: "이대로만 대답하면 면접 붙으실 것 같아요!",
+    interest_id: 1,
+    createdAt: "2022-04-08 00:00:00",
+    updatedAt: "2022-04-08 01:00:00",
+  },
+];
+
+const Comments = () => {
+  const [msg, setMsg] = useState("");
+  const [comments, setComments] = useState(dummyComments);
+
+  const handleButtonClick = (event) => {
+    const comment = {
+      id: shortid(),
+      nickname: nickname,
+      title: "new Comment",
+      content: msg,
+    };
+
+    const newComments = [comment, ...comments];
+    setComments(newComments);
+  };
+
+  const handleChangeMsg = (event) => {
+    setMsg(event.target.value);
+  };
+
+  const handleDeleteComment = (deleteIndex) => {
+    const restComments = comments.filter((idx) => idx !== deleteIndex);
+    setComments(restComments);
+  };
+
+  const CommentsRenderer = (comment, idx) => {
+    return (
+      <Comment
+        key={comment.id}
+        comment={comment}
+        handleDeleteComment={handleDeleteComment}
+        idx={idx}
+      />
+    );
+  };
+
+  return (
+    <>
+      <div className="CommentForm__container">
+        <div className="CommentForm__wrapper">
+          <div className="CommentForm__inputContainer">
+            <div className="CommentForm__inputWrapper">
+              <div className="CommentForm__input">
+                <textarea
+                  value={msg}
+                  onChange={handleChangeMsg}
+                  placeholder="댓글을 입력해주세요."
+                  className="CommentForm__input--message"
+                ></textarea>
+              </div>
+            </div>
+            <div className="CommentForm__submit">
+              <div className="CommentForm__submitIcon">
+                <button className="CommentForm__submitButton" onClick={handleButtonClick}>
+                  댓글 입력
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <ul className="Comments">{comments.map(CommentsRenderer)}</ul>
+    </>
+  );
+};
+
+export default Comments;
---
+++ client/src/pages/Article.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "../App.css";
+import styled from "styled-components";
+import Comments from "../components/Comments";
+
+// Article Page
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  border: 1px solid green;
+  height: 500px;
+  max-width: 1400px;
+  align-items: center;
+  margin: 100px auto 0 auto;
+`;
+
+const TeamName = styled.div`
+  width: 100%;
+  border: 1px solid red;
+  height: 100px;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
+const Content = styled.div`
+  height: 500px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  &:focus {
+    outline: none;
+  }
+`;
+const CommentWrap = styled.div``;
+
+function Article() {
+  return (
+    <Container>
+      <TeamName>팀네임</TeamName>
+      <Content />
+      <CommentWrap>
+        <Comments />
+      </CommentWrap>
+    </Container>
+  );
+}
+
+export default Article;
